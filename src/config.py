6b0ec2d4834204776a4eaa2cb8be3d90d8214398
AleYepes <73545779+AleYepes@users.noreply.github.com>
2025-08-16 00:43:54 +0200

    Edit config to loand .env vars correctly

--- src/config.py
+++ src/config.py
@@ -1,4 +1,10 @@
+# src/config.py
 import os
+from dotenv import load_dotenv
+
+project_root = os.path.join(os.path.dirname(__file__), '..')
+dotenv_path = os.path.join(project_root, '.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
